1541a1222bf3324376273e4e7fc9eaeb3733b6ca
unknown <leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.username Leandro>
2022-12-12 16:53:41 -0300

    heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getAllNotes } from './services/notes/getAllNotes';
+import { createNote } from './services/notes/createNote';
 
-function App() {
+const App = () => {
+  
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+
+  useEffect(() => {
+
+    getAllNotes()
+                .then(notes => {
+                  setNotes(notes);
+                });
+
+  }, []);
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    const noteToAdd = {
+      userId : notes.length + 1,
+      title : newNote,
+      body : newNote
+    }
+
+    createNote(noteToAdd)
+                .then( noteToAdd => {
+                  setNotes([...notes, noteToAdd]);
+                });
+
+    setNewNote('');
+
+  };
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {notes.map(note => {
+        return <ul key={note.id} >{note.id} - {note.title}</ul>
+      })}
+      <form onSubmit={handleSubmit} >
+        <input onChange={handleChange} value={newNote}/>
+        <button type='submit' >Agregar nota</button>
+      </form>
     </div>
   );
 }
